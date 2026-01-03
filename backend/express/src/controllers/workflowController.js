8d05542d05e656a08e6357f49a14aca335264ae2
Dev Kumar Singh <gohi772004@gmail.com>
2026-01-04 00:03:30 +0530

    feature get workflows and get single workflows

--- backend/express/src/controllers/workflowController.js
+++ backend/express/src/controllers/workflowController.js
@@ -41,4 +41,51 @@ const createWorkflow = async (req, res) => {
   }
 };
 
-module.exports = createWorkflow;
+const getworkflows = async (req, res) => {
+    try {
+        // Query the "Container" collection
+        // We select specific fields to keep the response light and fast.
+        const workflows = await Workflow.find()
+
+        res.json(workflows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+const getWorkflowById = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        // Step A: Find the Workflow Metadata
+        const workflow = await Workflow.findById(id);
+        if (!workflow) {
+            return res.status(404).json({ error: "Workflow not found" });
+        }
+
+        // Step B: Find the Logic (Nodes & Edges)
+        // We check if there is an active version pointer.
+        let flowData = { nodes: [], edges: [] };
+
+        if (workflow.active_version_id) {
+            const activeVersion = await WorkflowVersion.findById(workflow.active_version_id);
+            if (activeVersion) {
+                // Return the definition so React Flow can draw it
+                flowData = activeVersion.definition; 
+            }
+        }
+
+        // Step C: Return Combined Data
+        // The frontend gets the Name/ID (for the header) AND the Nodes (for the canvas)
+        res.json({
+            ...workflow.toObject(),
+            nodes: flowData.nodes || [],
+            edges: flowData.edges || []
+        });
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+module.exports = { createWorkflow, getworkflows, getWorkflowById };
--- backend/express/src/routes/workflowRoutes.js
+++ backend/express/src/routes/workflowRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const createWorkflow = require("../controllers/workflowController");
+const {createWorkflow, getworkflows, getWorkflowById} = require("../controllers/workflowController");
 
 router.post("/", createWorkflow);
+router.get("/", getworkflows);
+router.get("/:id", getWorkflowById);
+
 
 module.exports = router;
